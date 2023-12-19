513ef1a439223bd359ada24d0863c8499e689f9c
SwapnilVicky <swapnil.motoe@gmail.com>
2023-12-19 11:30:23 +0545

    mojito: overlay: move maintainer name into rro overlays
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -373,9 +373,6 @@ PRODUCT_PACKAGES += \
     TelephonyMojito \
     WifiMojito
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-bliss
-
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
