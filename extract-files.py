3726c51a5225caf2baeb55bf9d6cdd44aebe4f47
basamaryan <basam.aryan@gmail.com>
2025-04-15 18:58:55 +0545

    mojito: Switch to py extract utils Change-Id: Ibf4a46bc4e9dbb46e5e8474bebb07cd70edf109e
    Signed-off-by: romgharti <romgharti@gmail.com>

---
+++ extract-files.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/xiaomi/sm6150-common',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+         .regex_replace('sensors.touch.detect.so\n',''),
+    'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
+         .add_needed('libpiex_shim.so'),
+}
+
+module = ExtractUtilsModule(
+     'mojito',
+     'xiaomi',
+     blob_fixups=blob_fixups,
+     lib_fixups=lib_fixups,
+     namespace_imports=namespace_imports,
+ )
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm6150-common', module.vendor
+    )
+    utils.run()
