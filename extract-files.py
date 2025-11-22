c503e715d6370468ef557283fe7bc6914edff5b0
romgharti <romgharti@gmail.com>
2025-11-22 04:51:41 +0000

    mojito: Build vendor.qti.hardware.camera.device@1.0 from source
    Signed-off-by: romgharti <romgharti@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -29,9 +29,6 @@ def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (
-        'vendor.qti.hardware.camera.device@1.0',
-    ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
